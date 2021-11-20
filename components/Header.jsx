3256eb5af0e63100b5b30ba14b5a4dd3672cf43d
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-19 21:09:20 -0300

    Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,58 @@
+import styles from '../styles/Header.module.scss'
+
+import Link from 'next/link'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+
 export default function Header(props) {
+
+  const [stateMenuLateral, setStateMenuLateral] = useState(false);
+
   return(
-    <header>
-      <Link>
-        <a href=""></a>
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        <a>
+          <h1>Exame</h1>
+        </a>
       </Link>
+      <nav className={styles.nav}>
+        <FontAwesomeIcon onClick={() => setStateMenuLateral(!stateMenuLateral)} className={styles.icon} icon={faBars} />
+        <menu className={`${styles.menu} ${stateMenuLateral ? styles.active : ''}`}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link href="/">
+                <a>Opção o menu</a>
+              </Link>
+            </li>
+          </ul>
+        </menu>
+      </nav>
     </header>
   )
 }
