72876509272309f32fac1fac2396d503ac1752af
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-22 12:54:06 -0300

    Adicionando icones

--- components/Carrossel.jsx
+++ components/Carrossel.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import styles from '../styles/Carrossel.module.scss'
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Carrossel(props) {
 
@@ -7,8 +9,12 @@ export default function Carrossel(props) {
     
     return(
         <section className={styles.section}>
-            <div onClick={() => setCarroselAtual(carroselAtual -= 1)} className={styles.controller}>left</div>
-            <div onClick={() => setCarroselAtual(carroselAtual += 1)} className={styles.controller}>right</div>
+            <div onClick={() => setCarroselAtual(carroselAtual -= 1)} className={styles.controller}>
+                <FontAwesomeIcon className={styles.icon} icon={faAngleLeft} />
+            </div>
+            <div onClick={() => setCarroselAtual(carroselAtual += 1)} className={styles.controller}>
+                <FontAwesomeIcon className={styles.icon} icon={faAngleRight} />
+            </div>
             <div className={styles.areaImages}>
                 <div style={{transform: `translate(-${carroselAtual*100}vw)`}} className={styles.areaImage}>image 1</div>
                 <div style={{transform: `translate(-${carroselAtual*100}vw)`}} className={styles.areaImage}>image 2</div>
